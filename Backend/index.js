d73558030740459804d8b4f71e216974953e55f6
Anandhu_P_A <anandhupa131@gmail.com>
2023-09-13 09:02:47 +0530

    cors implemented in backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,14 +3,17 @@ const app = express();
 require("dotenv").config();
 const port = process.env.port || 4000;
 const axios  = require("axios");
-
+const cors = require("cors");
+app.use(cors());
 const OpenAI = require("openai");
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY , // defaults to process.env["OPENAI_API_KEY"]
 });
 
 
-
+app.get("/",async(req,res)=>{
+  res.send("Content creating application server")
+})
 app.get("/shayari",async(req,res)=>{
     try {
       let concept = req.query.c||"anything";
